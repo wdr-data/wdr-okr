0ec12eedc1df7af95ef4682f18f0e66f133889a9
P3nny <p3nny@hacking.studio>
2020-06-25 12:12:19 +0200

    ✨ Add youtube quintly data

---
+++ okr/models/youtube.py
@@ -0,0 +1,46 @@
+from django.db import models
+from .base import Product
+
+
+class YouTube(Product):
+    class Meta:
+        verbose_name = "YouTube-Account"
+        verbose_name_plural = "YouTube-Accounts"
+
+
+class YouTubeAnalytics(models.Model):
+    class Meta:
+        verbose_name = "YouTube-Analytic"
+        verbose_name_plural = "YouTube-Analytics"
+        unique_together = ("youtube", "time", "interval")
+
+    class Interval(models.TextChoices):
+        DAILY = "daily", "Täglich"
+        WEEKLY = "weekly", "Wöchentlich"
+        MONTHLY = "monthly", "Monatlich"
+
+    youtube = models.ForeignKey(
+        to=YouTube,
+        on_delete=models.CASCADE,
+        related_name="analytic",
+        related_query_name="analytics",
+    )
+    time = models.DateField(verbose_name="Datum")
+    interval = models.CharField(
+        verbose_name="Zeitraum", choices=Interval.choices, max_length=10
+    )
+
+    views = models.IntegerField(verbose_name="Views", null=True)
+    likes = models.IntegerField(verbose_name="Likes", null=True)
+    dislikes = models.IntegerField(verbose_name="Dislikes", null=True)
+    estimated_minutes_watched = models.IntegerField(
+        verbose_name="Gesehene Minuten (geschätzt)", null=True
+    )
+    average_view_duration = models.IntegerField(
+        verbose_name="Sehdauer im Schnitt", null=True
+    )
+
+    def __str__(self):
+        return (
+            f"{self.time}: {self.youtube.name} - {self.Interval(self.interval).label}"
+        )
---
+++ okr/views/youtube.py
@@ -0,0 +1,40 @@
+from datetime import date, datetime
+from pytz import timezone
+
+from django.http import HttpResponse
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
+
+from ..models.youtube import *
+from .. import quintly
+
+berlin = timezone("Europe/Berlin")
+
+
+@require_POST
+@csrf_exempt
+def trigger_analytics(request, interval):
+
+    for youtube in YouTube.objects.all():
+
+        df = quintly.get_youtube_analytics(
+            youtube.quintly_profile_id, interval=interval
+        )
+
+        for index, row in df.iterrows():
+            defaults = {
+                "views": row.views,
+                "likes": row.likes,
+                "dislikes": row.dislikes,
+                "estimated_minutes_watched": row.estimatedMinutesWatched,
+                "average_view_duration": row.averageViewDuration,
+            }
+
+            obj, created = YouTubeAnalytics.objects.update_or_create(
+                youtube=youtube,
+                time=date.fromisoformat(row.time),
+                interval=interval,
+                defaults=defaults,
+            )
+
+    return HttpResponse("ok")
