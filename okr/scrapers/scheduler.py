537f0184f95ed273abc18cb815d770215bb425cb
Jannes Höke <mail@jhoeke.de>
2020-07-22 11:44:12 +0200

    ♻️ Use APScheduler for Instagram and YouTube

--- okr/scrapers/scheduler.py
+++ okr/scrapers/scheduler.py
@@ -1,7 +1,11 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from django.conf import settings
+from pytz import timezone
 
-from . import podcasts
+from . import insta, youtube, podcasts
+
+
+berlin = timezone("Europe/Berlin")
 
 
 def start():
@@ -9,5 +13,51 @@ def start():
         return
 
     scheduler = BackgroundScheduler()
-    scheduler.add_job(podcasts.scrape_feed, trigger="interval", minutes=1)
+    scheduler.timezone = berlin
+
+    # Instagram
+    scheduler.add_job(
+        insta.scrape_insights,
+        args=["daily"],
+        trigger="cron",
+        hour="5,11,17,23",
+        minute="30",
+    )
+    scheduler.add_job(
+        insta.scrape_insights, args=["weekly"], trigger="cron", hour="6", minute="0",
+    )
+    scheduler.add_job(
+        insta.scrape_insights, args=["monthly"], trigger="cron", hour="6", minute="1",
+    )
+    scheduler.add_job(
+        insta.scrape_stories, trigger="cron", hour="5,11,17,23", minute="31",
+    )
+    scheduler.add_job(
+        insta.scrape_posts, trigger="cron", hour="5,11,17,23", minute="32",
+    )
+
+    # YouTube
+    scheduler.add_job(
+        youtube.scrape_analytics,
+        args=["daily"],
+        trigger="cron",
+        hour="5,11,17,23",
+        minute="35",
+    )
+    scheduler.add_job(
+        youtube.scrape_analytics, args=["weekly"], trigger="cron", hour="6", minute="5",
+    )
+    scheduler.add_job(
+        youtube.scrape_analytics,
+        args=["monthly"],
+        trigger="cron",
+        hour="6",
+        minute="6",
+    )
+
+    # Podcasts
+    scheduler.add_job(
+        podcasts.scrape_feed, trigger="cron", hour="5,11,17,23", minute="40"
+    )
+
     scheduler.start()
