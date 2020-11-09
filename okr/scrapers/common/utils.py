2a093e6e9ab0657e0ea6dad7ddde7c392c77f207
tcmetzger <ticon@gmx.net>
2020-11-09 00:07:09 -0800

    :bulb: Add docstrings to utils.py

--- okr/scrapers/common/utils.py
+++ okr/scrapers/common/utils.py
@@ -8,16 +8,34 @@ UTC = pytz.UTC
 
 
 def local_today() -> dt.date:
+    """Generate current date (Berlin time zone)
+
+    Returns:
+        dt.date: Today's date
+    """
     return dt.datetime.now(BERLIN).date()
 
 
 def local_yesterday() -> dt.date:
+    """Generate yesterday's date (Berlin time zone)
+
+    Returns:
+        dt.date: Yesterday's date
+    """
     return local_today() - dt.timedelta(days=1)
 
 
 def date_range(start: dt.date, end: dt.date) -> List[dt.date]:
+    """Generate a list of dates within a range. Start and end are both
+    inclusive.
+
+    Args:
+        start (dt.date): Start date for range
+        end (dt.date): End date for range
+
+    Returns:
+        List[dt.date]: List of dates between start and end
     """
-    start and end are both inclusive
-    """
+
     delta = (end - start).days
     return [start + dt.timedelta(days=delta_days) for delta_days in range(delta + 1)]
