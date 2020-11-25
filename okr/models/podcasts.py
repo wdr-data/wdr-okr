575bf46d3dc4967369178c5d1dd8fb1179758885
tcmetzger <ticon@gmx.net>
2020-11-24 20:03:34 -0800

    :bulb: podcast docstrings for database docs

--- okr/models/podcasts.py
+++ okr/models/podcasts.py
@@ -6,15 +6,15 @@ from .base import Product
 
 
 class Podcast(Product):
-    """Enthält Daten zu den einzelnen Podcast-Reihen, basierend auf Daten aus dem XML
-    feed and Spotify APIs.
+    """Enthält grundlegende Daten zu den einzelnen Podcast-Reihen, basierend auf Daten
+    aus dem XML-Feed und von Spotify.
 
     Die folgenden Tabellen nehmen auf die in der Spalte "id" vergebenen Schlüssel (als
-    foreign key namens "podcast_id") Bezug:
+    foreign key namens ``podcast``) Bezug:
 
     * :model:`okr.PodcastDataSpotify`
-    * :model:`PodcastDataSpotifyHourly`
-    * :model:`PodcastEpisode`
+    * :model:`okr.PodcastDataSpotifyHourly`
+    * :model:`okr.PodcastEpisode`
 
     """
 
@@ -61,38 +61,68 @@ class Podcast(Product):
 
 
 class PodcastDataSpotify(models.Model):
-    """Listener statistics on Podcast level, based on data from Spotify."""
+    """Daten zu Hörer*innen-Zahlen pro Podcast-Reihe, basierend auf Daten von Spotify.
+
+    Verknüpft mit :model:`okr.PodcastEpisode` über den foreign key ``podcast``.
+    """
 
     class Meta:
         """Model meta options."""
 
         db_table = "podcast_data_spotify"
-        verbose_name = "Podcast-Spotify-Nutzer"
-        verbose_name_plural = "Podcast-Spotify-Nutzer"
+        verbose_name = "Podcast-Spotify-Nutzer*innen"
+        verbose_name_plural = "Podcast-Spotify-Nutzer*innen"
         ordering = ["-date", "podcast"]
         unique_together = ["date", "podcast"]
 
-    date = models.DateField(verbose_name="Datum")
+    date = models.DateField(
+        verbose_name="Datum",
+        help_text="Erstellungsdatum des Datenpunkts",
+    )
     podcast = models.ForeignKey(
-        verbose_name="Podcast",
+        verbose_name="Podcast ID",
         to=Podcast,
         on_delete=models.CASCADE,
         related_name="data_spotify",
         related_query_name="data_spotify",
+        help_text="Globale ID der Podcast-Reihe",
     )
-    followers = models.IntegerField(verbose_name="Follower")
-    listeners = models.IntegerField(verbose_name="Listeners")
-    listeners_weekly = models.IntegerField(verbose_name="Listeners (wöchentlich)")
-    listeners_monthly = models.IntegerField(verbose_name="Listeners (monatlich)")
-    listeners_all_time = models.IntegerField(verbose_name="Listeners (insgesamt)")
-    last_updated = models.DateTimeField(verbose_name="Zuletzt upgedated", auto_now=True)
+    followers = models.IntegerField(
+        verbose_name="Follower",
+        help_text="Anzahl der Followers des Podcasts",
+    )
+    listeners = models.IntegerField(
+        verbose_name="Listeners",
+        help_text="Anzahl der Hörer*innen",
+    )
+    listeners_weekly = models.IntegerField(
+        verbose_name="Listeners (wöchentlich)",
+        help_text="Hörer*innen pro Woche",
+    )
+    listeners_monthly = models.IntegerField(
+        verbose_name="Listeners (monatlich)",
+        help_text="Hörer*innen pro Monat",
+    )
+    listeners_all_time = models.IntegerField(
+        verbose_name="Listeners (insgesamt)",
+        help_text="Hörer*innen insgesamt",
+    )
+    last_updated = models.DateTimeField(
+        verbose_name="Zuletzt upgedated",
+        help_text="Datum der letzten Daten-Aktualisierung",
+        auto_now=True,
+    )
 
     def __str__(self):
         return f"{self.podcast} - {self.date}"
 
 
 class PodcastDataSpotifyHourly(models.Model):
-    """Listening statistics on Podcast level, based on hourly Spotify data."""
+    """Daten über die stündliche Nutzung der Podcast-Reihen, die auf Spotify verfügbar
+    sind.
+
+    Verknüpft mit :model:`okr.PodcastEpisode` über den foreign key ``podcast``.
+    """
 
     class Meta:
         """Model meta options."""
@@ -103,23 +133,46 @@ class PodcastDataSpotifyHourly(models.Model):
         ordering = ["-date_time", "podcast"]
         unique_together = ["date_time", "podcast"]
 
-    date_time = models.DateTimeField(verbose_name="Zeitpunkt")
+    date_time = models.DateTimeField(
+        verbose_name="Zeitpunkt",
+        help_text="Datum und Uhrzeit des Datenpunktes",
+    )
     podcast = models.ForeignKey(
         verbose_name="Podcast",
         to=Podcast,
         on_delete=models.CASCADE,
         related_name="data_spotify_hourly",
         related_query_name="data_spotify_hourly",
+        help_text="Globale ID der Podcast-Reihe",
     )
-    starts = models.IntegerField(verbose_name="Starts")
-    streams = models.IntegerField(verbose_name="Streams")
+    starts = models.IntegerField(
+        verbose_name="Starts",
+        help_text="Anzahl der Starts",
+    )
+    streams = models.IntegerField(
+        verbose_name="Streams",
+        help_text="Anzahl der Streams",
+    )
 
     def __str__(self):
         return f"{self.podcast} - {self.date_time}"
 
 
 class PodcastEpisode(models.Model):
-    """Individual Podcast episodes, based on data from XML feed and Spotify APIs."""
+    """Daten zu den einzelnen Folgen der Podcasts, basierend auf Daten aus dem XML-Feed
+    und von Spotify.
+
+    Verknüpft mit :model:`okr.Podcast` über den foreign key ``podcast``.
+
+    Die folgenden Tabellen nehmen auf die in der Spalte "id" vergebenen Schlüssel (als
+    foreign key namens ``episode``) Bezug:
+
+    * :model:`okr.PodcastEpisodeDataPodstat`
+    * :model:`okr.PodcastEpisodeDataSpotify`
+    * :model:`okr.PodcastEpisodeDataSpotifyPerformance`
+    * :model:`okr.PodcastEpisodeDataSpotifyUser`
+    * :model:`okr.PodcastEpisodeDataWebtrekkPerformance`
+    """
 
     class Meta:
         """Model meta options."""
@@ -129,24 +182,55 @@ class PodcastEpisode(models.Model):
         verbose_name_plural = "Podcast-Episoden"
         ordering = ["-publication_date_time"]
 
-    title = models.CharField(max_length=400, verbose_name="Titel")
+    title = models.CharField(
+        max_length=400,
+        verbose_name="Titel",
+        help_text="Titel der einzelnen Folge",
+    )
     podcast = models.ForeignKey(
         verbose_name="Podcast",
         to=Podcast,
         on_delete=models.CASCADE,
         related_name="episodes",
         related_query_name="episode",
+        help_text="Globale ID der Podcast-Reihe",
     )
-    description = models.TextField(verbose_name="Beschreibung")
-    publication_date_time = models.DateTimeField(verbose_name="Veröffentlicht am")
-    media = models.URLField(max_length=1024, verbose_name="Media-URL")
-    zmdb_id = models.IntegerField(verbose_name="ZMDB-ID", unique=True)
+    description = models.TextField(
+        verbose_name="Beschreibung",
+        help_text="Beschreibungstext der Folge",
+    )
+    publication_date_time = models.DateTimeField(
+        verbose_name="Datum",
+        help_text="Veröffentlichungsdatum der Folge",
+    )
+    media = models.URLField(
+        max_length=1024,
+        verbose_name="Media-URL",
+        help_text="URL der Mediendatei",
+    )
+    zmdb_id = models.IntegerField(
+        verbose_name="ZMDB-ID",
+        help_text="ZMDB ID der Mediendatei",
+        unique=True,
+    )
 
-    spotify_id = models.CharField(max_length=32, verbose_name="Spotify ID", null=True)
+    spotify_id = models.CharField(
+        max_length=32,
+        verbose_name="Spotify ID",
+        help_text="Spotify ID, falls vorhanden",
+        null=True,
+    )
 
-    duration = models.DurationField(verbose_name="Audio-Länge")
+    duration = models.DurationField(
+        verbose_name="Länge",
+        help_text="Länge der Mediendatei",
+    )
 
-    last_updated = models.DateTimeField(verbose_name="Zuletzt upgedated", auto_now=True)
+    last_updated = models.DateTimeField(
+        verbose_name="Zuletzt upgedated",
+        help_text="Letzte Aktualisierung des Datenpunktes",
+        auto_now=True,
+    )
 
     def __str__(self):
         return (
@@ -155,7 +239,11 @@ class PodcastEpisode(models.Model):
 
 
 class PodcastEpisodeDataSpotify(models.Model):
-    """Listening statistics for individual Podcast episodes, based on data from Spotify."""
+    """Zusätzliche Daten zu einzelnen Podcast-Folgen. Nur für Podcasts, die auf Spotify
+    verfügbar sind.
+
+    Verknüpft mit :model:`okr.PodcastEpisode` über den foreign key ``episode``.
+    """
 
     class Meta:
         """Model meta options."""
@@ -166,27 +254,50 @@ class PodcastEpisodeDataSpotify(models.Model):
         unique_together = ("date", "episode")
         ordering = ["-date", "episode"]
 
-    date = models.DateField(verbose_name="Datum")
+    date = models.DateField(
+        verbose_name="Datum",
+        help_text="Datum der Abrufzahlen",
+    )
     episode = models.ForeignKey(
         verbose_name="Episode",
         to=PodcastEpisode,
         on_delete=models.CASCADE,
         related_name="data_spotify",
         related_query_name="data_spotify",
+        help_text="Globale ID der Episode",
     )
-    starts = models.IntegerField(verbose_name="Starts")
-    streams = models.IntegerField(verbose_name="Streams")
-    listeners = models.IntegerField(verbose_name="Listeners")
-    listeners_all_time = models.IntegerField(verbose_name="Listeners (insgesamt)")
+    starts = models.IntegerField(
+        verbose_name="Starts",
+        help_text="Anzahl der Starts",
+    )
+    streams = models.IntegerField(
+        verbose_name="Streams",
+        help_text="Anzahl der Streams",
+    )
+    listeners = models.IntegerField(
+        verbose_name="Listeners",
+        help_text="Hörer*innen für den jeweiligen Tag",
+    )
+    listeners_all_time = models.IntegerField(
+        verbose_name="Listeners (insgesamt)",
+        help_text="Gesamtzahl der Hörer*innen",
+    )
 
-    last_updated = models.DateTimeField(verbose_name="Zuletzt upgedated", auto_now=True)
+    last_updated = models.DateTimeField(
+        verbose_name="Zuletzt upgedated",
+        help_text="Zeitpunkt der Datenaktualisierung",
+        auto_now=True,
+    )
 
     def __str__(self):
         return f"{self.episode.title} ({self.date})"
 
 
 class PodcastEpisodeDataSpotifyUser(models.Model):
-    """Demographics data for individual Podcast episodes, based on data from Spotify."""
+    """Enthält demographische Daten von Spotify zu den auf Spotify verfügbaren Folgen.
+
+    Verknüpft mit :model:`okr.PodcastEpisode` über den foreign key ``episode``.
+    """
 
     class Meta:
         """Model meta options."""
@@ -197,37 +308,83 @@ class PodcastEpisodeDataSpotifyUser(models.Model):
         unique_together = ("date", "episode")
         ordering = ["-date", "episode"]
 
-    date = models.DateField(verbose_name="Datum")
+    date = models.DateField(
+        verbose_name="Datum",
+        help_text="Datum der Abrufzahlen",
+    )
     episode = models.ForeignKey(
         verbose_name="Episode",
         to=PodcastEpisode,
         on_delete=models.CASCADE,
         related_name="data_spotify_user",
         related_query_name="data_spotify_user",
+        help_text="Globale ID der Episode",
     )
-    age_0_17 = models.IntegerField(verbose_name="0-17 Jahre")
-    age_18_22 = models.IntegerField(verbose_name="18-22 Jahre")
-    age_23_27 = models.IntegerField(verbose_name="23-27 Jahre")
-    age_28_34 = models.IntegerField(verbose_name="28-34 Jahre")
-    age_35_44 = models.IntegerField(verbose_name="35-44 Jahre")
-    age_45_59 = models.IntegerField(verbose_name="45-59 Jahre")
-    age_60_150 = models.IntegerField(verbose_name="60-150 Jahre")
-    age_unknown = models.IntegerField(verbose_name="Unbekanntes Alter")
-    gender_female = models.IntegerField(verbose_name="Weiblich")
-    gender_male = models.IntegerField(verbose_name="Männlich")
-    gender_non_binary = models.IntegerField(verbose_name="Non_binary")
+    age_0_17 = models.IntegerField(
+        verbose_name="0-17",
+        help_text="Hörer*innen zwischen 0 und 17",
+    )
+    age_18_22 = models.IntegerField(
+        verbose_name="18-22",
+        help_text="Hörer*innen zwischen 18 und 22",
+    )
+    age_23_27 = models.IntegerField(
+        verbose_name="23-27",
+        help_text="Hörer*innen zwischen 23 und 27",
+    )
+    age_28_34 = models.IntegerField(
+        verbose_name="28-34",
+        help_text="Hörer*innen zwischen 28 und 34",
+    )
+    age_35_44 = models.IntegerField(
+        verbose_name="35-44",
+        help_text="Hörer*innen zwischen 35 und 44",
+    )
+    age_45_59 = models.IntegerField(
+        verbose_name="45-59",
+        help_text="Hörer*innen zwischen 45 und 59",
+    )
+    age_60_150 = models.IntegerField(
+        verbose_name="60-150",
+        help_text="Hörer*innen zwischen 60 und 150",
+    )
+    age_unknown = models.IntegerField(
+        verbose_name="Unbekannt",
+        help_text="Hörer*innen ohne Altersangabe",
+    )
+    gender_female = models.IntegerField(
+        verbose_name="Weiblich",
+        help_text="Anzahl der Hörerinnen",
+    )
+    gender_male = models.IntegerField(
+        verbose_name="Männlich",
+        help_text="Anzahl der Hörer",
+    )
+    gender_non_binary = models.IntegerField(
+        verbose_name="Non_binary",
+        help_text="Anzahl der non-binary Hörer*innen",
+    )
     gender_not_specified = models.IntegerField(
-        verbose_name="Geschlecht nicht angegeben"
+        verbose_name="Nicht angegeben",
+        help_text="Hörer*innen ohne Geschlechtsangabe",
     )
 
-    last_updated = models.DateTimeField(verbose_name="Zuletzt upgedated", auto_now=True)
+    last_updated = models.DateTimeField(
+        verbose_name="Zuletzt upgedated",
+        help_text="Zeitpunkt der Datenaktualisierung",
+        auto_now=True,
+    )
 
     def __str__(self):
         return f"{self.episode.title} ({self.date})"
 
 
 class PodcastEpisodeDataPodstat(models.Model):
-    """Listening statistics for individual Podcast episodes, based on data from Podstat."""
+    """Hörer*innenzahlen einzelner Podcast-Folgen, basierend auf Daten von
+        Podstat/Spotify.
+
+    Verknüpft mit :model:`okr.PodcastEpisode` über den foreign key ``episode``.
+    """
 
     class Meta:
         """Model meta options."""
@@ -238,25 +395,42 @@ class PodcastEpisodeDataPodstat(models.Model):
         unique_together = ("date", "episode")
         ordering = ["-date", "episode"]
 
-    date = models.DateField(verbose_name="Datum")
+    date = models.DateField(
+        verbose_name="Datum",
+        help_text="Datum der Abrufzahlen",
+    )
     episode = models.ForeignKey(
         verbose_name="Episode",
         to=PodcastEpisode,
         on_delete=models.CASCADE,
         related_name="data_podstat",
         related_query_name="data_podstat",
+        help_text="Globale ID der Episode",
     )
-    downloads = models.IntegerField(verbose_name="Downloads")
-    ondemand = models.IntegerField(verbose_name="OnDemand")
+    downloads = models.IntegerField(
+        verbose_name="Downloads",
+        help_text="Anzahl der Downloads",
+    )
+    ondemand = models.IntegerField(
+        verbose_name="OnDemand",
+        help_text="Anzahl der Ondemand-Abrufe",
+    )
 
-    last_updated = models.DateTimeField(verbose_name="Zuletzt upgedated", auto_now=True)
+    last_updated = models.DateTimeField(
+        verbose_name="Zuletzt upgedated",
+        help_text="Zeitpunkt der Datenaktualisierung",
+        auto_now=True,
+    )
 
     def __str__(self):
         return f"{self.episode.title} ({self.date})"
 
 
 class PodcastEpisodeDataSpotifyPerformance(models.Model):
-    """Performance data for individual Podcast episodes, based on Spotify data."""
+    """Performance-Daten zu den auf Spotify verfügbaren Folgen.
+
+    Verknüpft mit :model:`okr.PodcastEpisode` über den foreign key ``episode``.
+    """
 
     class Meta:
         """Model meta options."""
@@ -267,31 +441,54 @@ class PodcastEpisodeDataSpotifyPerformance(models.Model):
         unique_together = ("date", "episode")
         ordering = ["-date", "episode"]
 
-    date = models.DateField(verbose_name="Datum")
+    date = models.DateField(
+        verbose_name="Datum",
+        help_text="Datum der Performance-Daten",
+    )
     episode = models.ForeignKey(
         verbose_name="Episode",
         to=PodcastEpisode,
         on_delete=models.CASCADE,
         related_name="data_spotify_performance",
         related_query_name="data_spotify_performance",
+        help_text="Globale ID der Episode",
     )
     average_listen = models.DurationField(
         verbose_name="Average Listen",
         help_text="HH:MM:SS, 50% der Listeners hörten bis zu diesem Zeitpunkt.",
     )
-    quartile_1 = models.IntegerField(verbose_name="1. Quartil")
-    quartile_2 = models.IntegerField(verbose_name="2. Quartil")
-    quartile_3 = models.IntegerField(verbose_name="3. Quartil")
-    complete = models.IntegerField(verbose_name="Komplett")
+    quartile_1 = models.IntegerField(
+        verbose_name="1. Quartil",
+        help_text="Hörer*innen im 1. Viertel der Folge",
+    )
+    quartile_2 = models.IntegerField(
+        verbose_name="2. Quartil",
+        help_text="Hörer*innen im 2. Viertel der Folge",
+    )
+    quartile_3 = models.IntegerField(
+        verbose_name="3. Quartil",
+        help_text="Hörer*innen im 3. Viertel der Folge",
+    )
+    complete = models.IntegerField(
+        verbose_name="Komplett",
+        help_text="	Hörer*innen der gesamten Folge",
+    )
 
-    last_updated = models.DateTimeField(verbose_name="Zuletzt upgedated", auto_now=True)
+    last_updated = models.DateTimeField(
+        verbose_name="Zuletzt upgedated",
+        help_text="Zeitpunkt der Datenaktualisierung",
+        auto_now=True,
+    )
 
     def __str__(self):
         return f"{self.episode.title} ({self.date})"
 
 
 class PodcastEpisodeDataWebtrekkPerformance(models.Model):
-    """Performance data for individual Podcast episodes, based on Webtrekk data."""
+    """zusätzliche Abruf-Daten von Webtrekk für die einzelnen Folgen.
+
+    Verknüpft mit :model:`okr.PodcastEpisode` über den foreign key ``episode``.
+    """
 
     class Meta:
         """Model meta options."""
@@ -302,22 +499,36 @@ class PodcastEpisodeDataWebtrekkPerformance(models.Model):
         unique_together = ("date", "episode")
         ordering = ["-date", "episode"]
 
-    date = models.DateField(verbose_name="Datum")
+    date = models.DateField(
+        verbose_name="Datum",
+        help_text="Datum der Performance-Daten",
+    )
     episode = models.ForeignKey(
         verbose_name="Episode",
         to=PodcastEpisode,
         on_delete=models.CASCADE,
         related_name="data_webtrekk_performance",
         related_query_name="data_webtrekk_performance",
+        help_text="Globale ID der Episode",
     )
 
-    media_views = models.IntegerField(verbose_name="Medienansichten")
+    media_views = models.IntegerField(
+        verbose_name="Angefangen",
+        help_text="Angefangene Abrufe der Mediendatei",
+    )
     media_views_complete = models.IntegerField(
-        verbose_name="Medienansichten vollständig"
+        verbose_name="Vollständig",
+        help_text="Abgeschlossene Abrufe der Mediendatei",
     )
-    playing_time = models.DurationField(verbose_name="Spieldauer", help_text="HH:MM:SS")
+    playing_time = models.DurationField(
+        verbose_name="Spieldauer", help_text="Abspieldauer der Mediendatei (HH:MM:SS)"
+    )
 
-    last_updated = models.DateTimeField(verbose_name="Zuletzt upgedated", auto_now=True)
+    last_updated = models.DateTimeField(
+        verbose_name="Zuletzt upgedated",
+        help_text="Zeitpunkt der Datenaktualisierung",
+        auto_now=True,
+    )
 
     def __str__(self):
         return f"{self.episode.title} ({self.date})"
