57c9f40d4a098c4a009a1a3466c79a886708807a
tcmetzger <ticon@gmx.net>
2020-12-09 15:14:35 -0800

    :sparkles: add info and links on foreign keys

--- docs/database_tables.py
+++ docs/database_tables.py
@@ -45,15 +45,40 @@ def build_html(app_labels: list, html_top: str, html_bottom: str) -> str:
                     description = field["help_text"]
                 else:
                     description = field["verbose_name"]
-                table_fields.append([field["name"], field["db_type"], description])
 
+                if field["relation"]:
+                    data_type = f'{field["db_type"]} ({field["internal_type"]})'
+                else:
+                    data_type = f'{field["db_type"]}'
+
+                table_fields.append([field["name"], data_type, description])
+
+            # only include tables that are stored in db
             if (
                 db_table["fields"][0]["name"] == "id"
                 and db_table["fields"][0]["type"] == "AutoField"
             ):
+                # create table info text from docstring
+                docstring_html = db_table["docstring"].replace("\n", "<br />\n")
+                info_text = f"<p>{docstring_html}</p>"
+
+                # if table uses foreign keys: create a list of foreign keys with links
+                if db_table["relations"]:
+                    foreign_key_text = ""
+                    for relation in db_table["relations"]:
+                        if relation["type"] == "ForeignKey":
+                            foreign_key_text += f'<li>Den Foreign Key <a href="#{relation["target"]}">"{relation["name"]}"</a></li>'
+                    if foreign_key_text:
+                        info_text += "<p>Diese Tabelle nutzt folgende Foreign Keys:</p>"
+                        info_text += "<ul>"
+                        info_text += foreign_key_text
+                        info_text += "</ul>"
+
+                # combine table name, table info text, table fields, and Django model name
                 output_table_dict[db_table["db_table_name"]] = [
-                    db_table["docstring"],
+                    info_text,
                     table_fields,
+                    db_table["name"],
                 ]
 
     # sort dict of database tables alphabetically
@@ -62,9 +87,9 @@ def build_html(app_labels: list, html_top: str, html_bottom: str) -> str:
     # collect HTML items in a string
     html_tables = ""
     for table_name, table_infos in output_sorted.items():
-        # embed output table in HTML
+        # convert output table to HTML
         html_tables += (
-            f"<h3>Database Tabellenname: {table_name}</h3>"
+            f"<h3><a name='{table_infos[2]}'>Database Tabellenname: {table_name}</a></h3>"
             + f"<div class='docstring'>{table_infos[0]}</div>"
             + "\n"
             + tabulate(
