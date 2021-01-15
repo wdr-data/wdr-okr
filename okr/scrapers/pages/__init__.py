fefe00fbfcaa0cb4286cd65efe68282b2182892b
tcmetzger <ticon@gmx.net>
2021-01-14 21:05:38 -0800

    ✨ Add support for image galleries

--- okr/scrapers/pages/__init__.py
+++ okr/scrapers/pages/__init__.py
@@ -405,10 +405,18 @@ def _handle_sophora_document(
     # count the number of words in the body text (copytext and subheadlines)
     all_words = ""
     paragraph_filter = ["copytext", "headline"]
-    for paragraph in sophora_document_info["detail"]["messageBody"]:
-        if paragraph["paragraphType"] in paragraph_filter:
-            all_words += paragraph["paragraphValue"] + " "
-    word_count = len(all_words.strip().split(" "))
+    if "detail" in sophora_document_info.keys():
+        for paragraph in sophora_document_info["detail"]["messageBody"]:
+            if paragraph["paragraphType"] in paragraph_filter:
+                all_words += paragraph["paragraphValue"] + " "
+    elif "galleryBody" in sophora_document_info.keys():
+        for paragraph in sophora_document_info["galleryBody"]:
+            if paragraph["paragraphType"] in paragraph_filter:
+                all_words += paragraph["paragraphValue"] + " "
+    if len(all_words) > 0:
+        word_count = len(all_words.strip().split(" "))
+    else:
+        word_count= 0
 
     SophoraDocumentMeta.objects.get_or_create(
         sophora_document=sophora_document,
