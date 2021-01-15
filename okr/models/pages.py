ea4d130b8c7543f727839eaddf77f346a461b976
Jannes Höke <mail@jhoeke.de>
2021-01-15 11:00:33 +0100

    🗃 Allow NULL for word_count

--- okr/models/pages.py
+++ okr/models/pages.py
@@ -198,6 +198,7 @@ class SophoraDocumentMeta(models.Model):
     )
     word_count = models.IntegerField(
         verbose_name="Word Count",
+        null=True,
         help_text="Anzahl der Wörter im Fließtext (incl. Zwischenüberschriften)",
     )
     document_type = models.CharField(
