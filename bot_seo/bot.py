587797484779847bf9c7b1fc7d308db9b3e8bc38
tcmetzger <ticon@gmx.net>
2021-03-25 22:08:04 -0700

    ✨ Send message via Teams webhook

--- bot_seo/bot.py
+++ bot_seo/bot.py
@@ -1,5 +1,4 @@
 import datetime as dt
-import json
 import os
 
 from django.db.models import F, Sum
@@ -9,12 +8,14 @@ from okr.models.pages import *
 from okr.scrapers.common.utils import (
     local_yesterday,
     local_today,
-    BERLIN,
 )
+
+URL = os.environ.get("TEAMS_WEBHOOK")
 
 
 def _get_pages(impressions_limit: int = 10000, date: dt.date = None) -> Page:
-    gsc_yesterday = (
+    # get all pages that had a certain number of impressions on a certain date.
+    gsc_date = (
         Page.objects.filter(data_gsc__date=date)
         .annotate(impressions_all=Sum(F("data_gsc__impressions")))
         .filter(
@@ -23,10 +24,12 @@ def _get_pages(impressions_limit: int = 10000, date: dt.date = None) -> Page:
         .order_by(F("impressions_all").desc(nulls_last=True))
     )
 
-    return gsc_yesterday
+    return gsc_date
 
 
-def get_seo_articles_to_update(impressions_limit: int = 10000, date: dt.date = None):
+def _get_seo_articles_to_update(impressions_limit: int = 10000, date: dt.date = None):
+    # generate a list of pages that had at least a certain number of impressions
+    # on a certain date and have not been updated today.
     pages = _get_pages(impressions_limit, date)
 
     articles_to_do = []
@@ -47,9 +50,63 @@ def get_seo_articles_to_update(impressions_limit: int = 10000, date: dt.date = N
         if latest_meta.editorial_update.date() == local_today():
             continue
 
+        # add data from latest_meta to page object
+        page.latest_meta = latest_meta
         articles_to_do.append(page)
 
         if len(articles_to_do) == 5:
             break
 
     return articles_to_do
+
+
+def _generate_to_do_strings(articles: list) -> list:
+    to_dos = []
+
+    for article in articles:
+        impressions = article.impressions_all
+        headline = article.latest_meta.headline
+        url = article.url
+        to_dos.append(
+            f'* Der Beitrag **"[{headline}]({url})"** hatte gestern **{"{:,}".format(impressions).replace(",",".")}** Suchmaschinen-Impressions und wurde heute noch nicht aktualisiert.'
+        )
+
+    return to_dos
+
+
+def _generate_json_payload(messages: str) -> dict:
+    # generate json payload to send to Teams
+    payload = {
+        "@type": "MessageCard",
+        "@context": "http://schema.org/extensions",
+        "summary": "Markup test",
+        "sections": [
+            {
+                "title": "Hallo! Ich habe mir mal die Beiträge von gestern in der Search Console angesehen und habe folgende Vorschläge:"
+            }
+        ],
+    }
+
+    for message in messages:
+        payload["sections"].append({"text": message})
+
+    return payload
+
+
+def _send_to_teams(payload: dict) -> requests.models.Response:
+    return requests.post(URL, json=payload).raise_for_status()
+
+
+def bot_seo():
+    # Generate list of Page objects that are potential to-do items
+    articles_to_do = _get_seo_articles_to_update(10000, local_yesterday())
+
+    # Generate a list of sentences about the to-do items
+    to_dos = _generate_to_do_strings(articles_to_do)
+
+    # Generate payload for to-dos
+    payload = _generate_json_payload(to_dos)
+
+    # Send payload to MS Teams
+    result = _send_to_teams(payload)
+    print(result)
