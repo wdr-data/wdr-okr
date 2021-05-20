68264960f8ee14582b1065b29312f27df5791993
tcmetzger <ticon@gmx.net>
2021-05-19 21:48:20 -0700

    ✨ Add Twitter trends top 10

--- bot_seo/checkup/bot.py
+++ bot_seo/checkup/bot.py
@@ -1,8 +1,10 @@
 """ SEO Bot for to dos """
 
 import os
+from typing import List
 
 from loguru import logger
+import tweepy
 
 from .pytrends_patch import TrendReq
 from .teams_message import _generate_adaptive_card
@@ -10,6 +12,8 @@ from ..teams_tools import generate_teams_payload, send_to_teams
 from okr.scrapers.common.types import JSON
 
 WEBHOOK_URL = os.environ.get("TEAMS_WEBHOOK_SEO_BOT")
+TWITTER_API_KEY = os.environ.get("TWITTER_API_KEY")
+TWITTER_API_SECRET = os.environ.get("TWITTER_API_SECRET")
 
 
 def _get_google_trends() -> JSON:
@@ -17,11 +21,23 @@ def _get_google_trends() -> JSON:
     return pytrends.realtime_trending_searches()
 
 
+def _get_twitter_trends(woid: str = "23424829") -> List[JSON]:
+    auth = tweepy.OAuthHandler(TWITTER_API_KEY, TWITTER_API_SECRET)
+    api = tweepy.API(auth)
+    result = api.trends_place(woid)[0]
+    return result["trends"]
+
+
 def run():
     google_trends_data = _get_google_trends()
+    twitter_trends_data = _get_twitter_trends()
 
-    adaptive_card = _generate_adaptive_card(google_trends_data)
+    adaptive_card = _generate_adaptive_card(google_trends_data, twitter_trends_data)
     payload = generate_teams_payload(adaptive_card)
+
+    import json
+
+    print(json.dumps(payload["attachments"][0]["content"]))
 
     # Send payload to MS Teams
     result = send_to_teams(payload, WEBHOOK_URL)
--- bot_seo/checkup/teams_message.py
+++ bot_seo/checkup/teams_message.py
@@ -10,47 +10,78 @@ from pyadaptivecards.actions import OpenUrl
 
 from okr.scrapers.common.types import JSON
 from ..pyadaptivecards_tools import Container, Column
+from ..teams_tools import format_number
 
 GREETINGS = ["Hallo!", "Guten Tag!", "Hi!"]
 MORE_URL = os.environ.get("SEO_BOT_TODO_MORE_URL")
 
 
-def _generate_adaptive_card(google_trends_data: List[JSON]) -> AdaptiveCard:
+def _generate_adaptive_card(
+    google_trends_data: List[JSON] = None,
+    twitter_trends_data: List[JSON] = None,
+) -> AdaptiveCard:
     title = TextBlock("SEO-Checkup", size="large", weight="bolder")
+    card_body = [title]
 
-    title_trends = TextBlock("Google Echtzeit-Trends", size="medium", weight="bolder")
-
-    trends_items = []
-    for trend_data in google_trends_data:
-        topic = TextBlock(" • ".join(trend_data["entityNames"]), weight="bolder")
-
-        top_article = trend_data["articles"][0]
-        article_url = f"[{top_article['source']}]({top_article['url']})"
-        time = top_article["time"]
-        share_url = f"[Mehr]({trend_data['shareUrl']})"
-        subtext = TextBlock(f"{article_url} {time} {share_url}")
-
-        image = Image(
-            f"https:{trend_data['image']['imgUrl']}",
-            selectAction=OpenUrl(
-                url=trend_data["image"]["newsUrl"],
-                title=trend_data["image"]["source"],
-            ),
+    if google_trends_data:
+        title_trends = TextBlock(
+            "Google Echtzeit-Trends", size="medium", weight="bolder"
         )
+        card_body.append(title_trends)
 
-        trends_items.append(
-            ColumnSet(
-                columns=[
-                    Column(
-                        items=[topic, subtext],
-                        width=80,
-                        verticalContentAlignment="center",
-                    ),
-                    Column(items=[image], width=20),
-                ]
+        trends_items = []
+        for trend_data in google_trends_data:
+            topic = TextBlock(" • ".join(trend_data["entityNames"]), weight="bolder")
+
+            top_article = trend_data["articles"][0]
+            article_url = f"[{top_article['source']}]({top_article['url']})"
+            time = top_article["time"]
+            share_url = f"[Mehr]({trend_data['shareUrl']})"
+            subtext = TextBlock(f"{article_url} {time} {share_url}")
+
+            image = Image(
+                f"https:{trend_data['image']['imgUrl']}",
+                selectAction=OpenUrl(
+                    url=trend_data["image"]["newsUrl"],
+                    title=trend_data["image"]["source"],
+                ),
             )
-        )
 
-    card = AdaptiveCard(body=[title, title_trends, Container(trends_items)])
+            trends_items.append(
+                ColumnSet(
+                    columns=[
+                        Column(
+                            items=[topic, subtext],
+                            width=80,
+                            verticalContentAlignment="center",
+                        ),
+                        Column(items=[image], width=20),
+                    ]
+                )
+            )
+
+        card_body.append(Container(trends_items))
+
+    if twitter_trends_data:
+        title_trends = Container(
+            [TextBlock("Twitter-Trends Deutschland", size="medium", weight="bolder")],
+            separator=True,
+        )
+        card_body.append(title_trends)
+
+        twitter_trends_items = []
+        i = 0
+        while i < 10:
+            # for item in twitter_trends_data:
+            item = twitter_trends_data[i]
+            i += 1
+            trend_item = f"{i}. [{item['name']}]({item['url']})"
+            if item["tweet_volume"]:  # can be None
+                trend_item += f" ({format_number(item['tweet_volume'])} Tweets)"
+            twitter_trends_items.append(TextBlock(trend_item))
+
+        card_body.append(Container(twitter_trends_items))
+
+    card = AdaptiveCard(body=card_body)
 
     return card
