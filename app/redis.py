cba4641de332ee044e700fbb7ec1719907f8b558
Jannes Höke <mail@jhoeke.de>
2021-06-22 22:56:01 +0200

    🐛 Disable certificate validation for Redis connection

--- app/redis.py
+++ app/redis.py
@@ -5,9 +5,12 @@ import os
 import redis
 from rq.queue import Queue
 
-redis_url = os.getenv(
+REDIS_URL = os.getenv(
     "REDIS_TLS_URL",
     os.getenv("REDIS_URL", "redis://localhost:6379"),
 )
-conn = redis.from_url(redis_url)
+conn = redis.from_url(
+    REDIS_URL,
+    ssl_cert_reqs=None,
+)
 q = Queue(connection=conn)
