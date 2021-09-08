f74e410a5476a4d76809d089f6498cd905cc65e6
Jannes Höke <mail@jhoeke.de>
2021-09-08 19:55:26 +0200

    ✨ Add support for BigQuery

--- okr/scrapers/common/google.py
+++ okr/scrapers/common/google.py
@@ -4,6 +4,7 @@ Requires a ``google-credentials.json`` file in the root directory.
 
 from loguru import logger
 from apiclient.discovery import build
+from google.cloud import bigquery
 from google.oauth2 import service_account
 
 KEY_PATH = "google-credentials.json"
@@ -11,12 +12,22 @@ KEY_PATH = "google-credentials.json"
 try:
     credentials = service_account.Credentials.from_service_account_file(
         KEY_PATH,
-        scopes=["https://www.googleapis.com/auth/webmasters"],
+        scopes=[
+            "https://www.googleapis.com/auth/webmasters",
+            "https://www.googleapis.com/auth/cloud-platform",
+        ],
     )
 
 except FileNotFoundError:
-    logger.warning("Service account file not found, GSC-related scrapers will fail")
+    logger.warning(
+        "Service account file not found, GSC/BigQuery-related scrapers will fail"
+    )
     webmasters_service = None
+    bigquery_client = None
 
 else:
     webmasters_service = build("webmasters", "v3", credentials=credentials)
+    bigquery_client = bigquery.Client(
+        credentials=credentials,
+        project=credentials.project_id,
+    )
