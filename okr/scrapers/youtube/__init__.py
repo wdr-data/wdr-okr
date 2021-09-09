86b1634df95ea44be52184434893fd0763bd0aa0
Jannes Höke <mail@jhoeke.de>
2021-09-09 23:41:23 +0200

    ✨ Add first scrapers for BigQuery

--- okr/scrapers/youtube/__init__.py
+++ okr/scrapers/youtube/__init__.py
@@ -2,7 +2,7 @@
 
 import datetime as dt
 from time import sleep
-from typing import Optional
+from typing import Dict, Optional
 
 from django.db.utils import IntegrityError
 from django.db.models import Q
@@ -13,9 +13,11 @@ from ...models.youtube import (
     YouTube,
     YouTubeAnalytics,
     YouTubeVideo,
+    YouTubeVideoAnalytics,
+    YouTubeVideoTrafficSource,
 )
-from . import quintly
-from ..common.utils import BERLIN, to_timedelta
+from . import quintly, google
+from ..common.utils import BERLIN, local_today, to_timedelta
 
 
 def scrape_full(youtube: YouTube):
@@ -31,8 +33,10 @@ def scrape_full(youtube: YouTube):
 
     sleep(1)
 
-    scrape_channel_analytics(start_date=start_date, youtube_filter=youtube_filter)
-    scrape_video_analytics(start_date=start_date, youtube_filter=youtube_filter)
+    # scrape_channel_analytics(start_date=start_date, youtube_filter=youtube_filter)
+    # scrape_videos(start_date=start_date, youtube_filter=youtube_filter)
+    # scrape_video_analytics(start_date=start_date, youtube_filter=youtube_filter)
+    scrape_video_traffic_sources(start_date=start_date, youtube_filter=youtube_filter)
 
     logger.success("Finished full YouTube scrape of {}", youtube)
 
@@ -103,7 +107,7 @@ def scrape_channel_analytics(
                 )
 
 
-def scrape_video_analytics(
+def scrape_videos(
     *,
     start_date: Optional[dt.date] = None,
     youtube_filter: Optional[Q] = None,
@@ -172,5 +176,159 @@ def scrape_video_analytics(
                 logger.exception(
                     "Data for basic YouTube video data for {} failed integrity check:\n{}",
                     row.title,
+                    defaults,
+                )
+
+
+def scrape_video_analytics(
+    *,
+    start_date: Optional[dt.date] = None,
+    end_date: Optional[dt.date] = None,
+    youtube_filter: Optional[Q] = None,
+):
+    """Read YouTube video analytics data from BigQuery and store in database.
+
+    Results are saved in
+    :class:`~okr.models.youtube.YouTubeVideoAnalytics`.
+
+    Args:
+        start_date (Optional[date], optional): Earliest data to request data for.
+            Defaults to None.
+        end_date (Optional[date], optional): Latest data to request data for.
+            Defaults to None.
+        youtube_filter (Optional[Q], optional): Q object to filter data with.
+            Defaults to None.
+    """
+    youtubes = YouTube.objects.all()
+
+    if youtube_filter:
+        youtubes = youtubes.filter(youtube_filter)
+
+    for youtube in youtubes:
+        logger.debug("Scraping YouTube video analytics for {}", youtube)
+
+        # Cache videos to prevent multiple queries for the same video
+        video_cache: Dict[str, YouTubeVideo] = {}
+
+        rows_iter = google.get_bigquery_basic(
+            youtube.bigquery_suffix,
+            start_date=start_date,
+            end_date=end_date,
+        )
+
+        for row in rows_iter:
+            defaults = {
+                "views": row.views,
+                "likes": row.likes,
+                "dislikes": row.dislikes,
+                "comments": row.comments,
+                "shares": row.shares,
+                "subscribers_gained": row.subscribers_gained,
+                "subscribers_lost": row.subscribers_lost,
+                "watch_time": to_timedelta(row.watch_time_minutes * 60),
+            }
+
+            # Find video in cache or query database
+            try:
+                youtube_video = video_cache[row.video_id]
+            except KeyError:
+                youtube_video = YouTubeVideo.objects.filter(
+                    external_id=row.video_id,
+                ).first()
+
+                video_cache[row.video_id] = youtube_video
+
+            if youtube_video is None:
+                logger.warning("Video {} not found in database", row.video_id)
+                continue
+
+            try:
+                YouTubeVideoAnalytics.objects.update_or_create(
+                    youtube_video=youtube_video,
+                    date=row.date,
+                    live_or_on_demand=row.live_or_on_demand,
+                    defaults=defaults,
+                )
+            except IntegrityError as e:
+                capture_exception(e)
+                logger.exception(
+                    "Data for video analytics at {} failed integrity check:\n{}",
+                    row.time,
+                    defaults,
+                )
+
+
+def scrape_video_traffic_sources(
+    *,
+    start_date: Optional[dt.date] = None,
+    end_date: Optional[dt.date] = None,
+    youtube_filter: Optional[Q] = None,
+):
+    """Read YouTube video traffic source data from BigQuery and store in database.
+
+    Results are saved in
+    :class:`~okr.models.youtube.YouTubeVideoTrafficSource`.
+
+    Args:
+        start_date (Optional[date], optional): Earliest data to request data for.
+            This date refers to the partition field value, not the date of the
+            data itself. Defaults to None. If None, the start date will be
+            set to 6 months in the past.
+        end_date (Optional[date], optional): Latest data to request data for.
+            Defaults to None.
+        youtube_filter (Optional[Q], optional): Q object to filter data with.
+            Defaults to None.
+    """
+    youtubes = YouTube.objects.all()
+
+    if youtube_filter:
+        youtubes = youtubes.filter(youtube_filter)
+
+    if start_date is None:
+        start_date = local_today() - dt.timedelta(days=31 * 6)
+
+    for youtube in youtubes:
+        logger.debug("Scraping YouTube video traffic source data for {}", youtube)
+
+        rows_iter = google.get_bigquery_traffic_source(
+            youtube.bigquery_suffix,
+            start_date,
+            end_date=end_date,
+        )
+
+        for row in rows_iter:
+            defaults = {
+                "views": row.views,
+                "watch_time": to_timedelta(row.watch_time_minutes * 60),
+            }
+
+            youtube_video = YouTubeVideo.objects.filter(
+                external_id=row.video_id,
+            ).first()
+
+            if youtube_video is None:
+                logger.warning("Video {} not found in database", row.video_id)
+                continue
+            elif youtube_video.published_at.date() < start_date:
+                logger.debug(
+                    "Video {} published before start date {}, skipping to prevent overwriting with bad data",
+                    youtube_video,
+                    start_date,
+                )
+                continue
+
+            try:
+                YouTubeVideoTrafficSource.objects.update_or_create(
+                    youtube_video=youtube_video,
+                    source_type=YouTubeVideoTrafficSource.SourceType[
+                        f"SOURCE_TYPE_{row.traffic_source_type}"
+                    ],
+                    defaults=defaults,
+                )
+            except IntegrityError as e:
+                capture_exception(e)
+                logger.exception(
+                    "Data for video analytics at {} failed integrity check:\n{}",
+                    row.time,
                     defaults,
                 )
--- okr/scrapers/youtube/google.py
+++ okr/scrapers/youtube/google.py
@@ -1,6 +1,6 @@
 """ Methods for scraping YouTube data with Quintly """
 
-import datetime
+import datetime as dt
 from typing import Generator, Optional
 
 from google.cloud import bigquery
@@ -14,28 +14,28 @@ from ..common.google import bigquery_client, insert_table_name, iter_results
 def get_bigquery_basic(
     bigquery_suffix: str,
     *,
-    start_date: Optional[datetime.date] = None,
-    end_date: Optional[datetime.date] = None,
+    start_date: Optional[dt.date] = None,
+    end_date: Optional[dt.date] = None,
 ) -> Generator[pd.Series, None, None]:
     """Read YouTube Video data from BigQuery.
 
     Args:
         bigquery_suffix (str): BigQuery dataset/table suffix.
-        start_date (Optional[datetime.date], optional): Date of earliest data to
+        start_date (Optional[dt.date], optional): Date of earliest data to
           request. This date refers to the partition field value, not the date
           of the data itself. Defaults to None. Will be set to 7 days if None.
-        end_date (Optional[datetime.date], optional): Date of latest data to
+        end_date (Optional[dt.date], optional): Date of latest data to
           request. This date refers to the partition field value, not the date
           of the data itself. Defaults to None. Will be set to today if None.
 
-    Yields:
-        pd.Series: BigQuery response data.
+    Returns:
+        pd.DataFrame: BigQuery response data.
     """
 
     today = utils.local_today()
 
     if start_date is None:
-        start_date = today - datetime.timedelta(days=7)
+        start_date = today - dt.timedelta(days=7)
 
     if end_date is None:
         end_date = today
@@ -76,10 +76,199 @@ GROUP BY
     )
 
     def df_cleaner(df: pd.DataFrame) -> pd.DataFrame:
+        # Convert to date
         df.date = pd.to_datetime(df.date).dt.date
-        df = df.replace({np.nan: None})
+        return df.replace({np.nan: None})
 
-        return df
+    yield from iter_results(
+        bigquery_client,
+        query,
+        job_config,
+        df_cleaner,
+    )
+
+
+def get_bigquery_traffic_source(
+    bigquery_suffix: str,
+    start_date: dt.date,
+    *,
+    end_date: Optional[dt.date] = None,
+) -> Generator[pd.Series, None, None]:
+    """Read YouTube Video traffic source data from BigQuery.
+
+    Args:
+        bigquery_suffix (str): BigQuery dataset/table suffix.
+        start_date (dt.date): Date of earliest data to
+          request. This date refers to the partition field value, not the date
+          of the data itself.
+        end_date (Optional[dt.date], optional): Date of latest data to
+          request. This date refers to the partition field value, not the date
+          of the data itself. Defaults to None. Will be set to today if None.
+
+    Yields:
+        pd.Series: BigQuery response data.
+    """
+
+    if end_date is None:
+        end_date = utils.local_today()
+
+    query = """
+SELECT
+  `video_id`,
+  `traffic_source_type`,
+  SUM(`views`) AS `views`,
+  SUM(`watch_time_minutes`) AS `watch_time_minutes`
+FROM
+  `@table_name`
+WHERE
+  DATE(_PARTITIONTIME) >= @start_date
+  AND DATE(_PARTITIONTIME) <= @end_date
+  AND `video_id` IS NOT NULL
+GROUP BY
+  `video_id`,
+  `traffic_source_type`
+    """.strip()
+
+    query = insert_table_name(query, "p_channel_traffic_source_a2_", bigquery_suffix)
+
+    job_config = bigquery.QueryJobConfig(
+        default_dataset=f"wdr-okr.youtube_channel_{bigquery_suffix}",
+        query_parameters=[
+            bigquery.ScalarQueryParameter("start_date", "DATE", start_date.isoformat()),
+            bigquery.ScalarQueryParameter("end_date", "DATE", end_date.isoformat()),
+        ],
+    )
+
+    def df_cleaner(df: pd.DataFrame) -> pd.DataFrame:
+        return df.replace({np.nan: None})
+
+    yield from iter_results(
+        bigquery_client,
+        query,
+        job_config,
+        df_cleaner,
+    )
+
+
+def get_bigquery_search_terms(
+    bigquery_suffix: str,
+    start_date: dt.date,
+    *,
+    end_date: Optional[dt.date] = None,
+) -> Generator[pd.Series, None, None]:
+    """Read YouTube Video search term data from BigQuery.
+
+    Args:
+        bigquery_suffix (str): BigQuery dataset/table suffix.
+        start_date (dt.date): Date of earliest data to
+          request. This date refers to the partition field value, not the date
+          of the data itself.
+        end_date (Optional[dt.date], optional): Date of latest data to
+          request. This date refers to the partition field value, not the date
+          of the data itself. Defaults to None. Will be set to today if None.
+
+    Yields:
+        pd.Series: BigQuery response data.
+    """
+
+    if end_date is None:
+        end_date = utils.local_today()
+
+    query = """
+SELECT
+  `video_id`,
+  `traffic_source_detail`,
+  SUM(`views`) AS `views`,
+  SUM(`watch_time_minutes`) AS `watch_time_minutes`
+FROM
+  `@table_name`
+WHERE
+  DATE(_PARTITIONTIME) >= @start_date
+  AND DATE(_PARTITIONTIME) <= @end_date
+  AND `traffic_source_type` = 5
+  AND `traffic_source_detail` IS NOT NULL
+  AND `video_id` IS NOT NULL
+GROUP BY
+  `video_id`,
+  `traffic_source_detail`
+    """.strip()
+
+    query = insert_table_name(query, "p_channel_traffic_source_a2_", bigquery_suffix)
+
+    job_config = bigquery.QueryJobConfig(
+        default_dataset=f"wdr-okr.youtube_channel_{bigquery_suffix}",
+        query_parameters=[
+            bigquery.ScalarQueryParameter("start_date", "DATE", start_date.isoformat()),
+            bigquery.ScalarQueryParameter("end_date", "DATE", end_date.isoformat()),
+        ],
+    )
+
+    def df_cleaner(df: pd.DataFrame) -> pd.DataFrame:
+        return df.replace({np.nan: None})
+
+    yield from iter_results(
+        bigquery_client,
+        query,
+        job_config,
+        df_cleaner,
+    )
+
+
+def get_bigquery_external_traffic(
+    bigquery_suffix: str,
+    start_date: dt.date,
+    *,
+    end_date: Optional[dt.date] = None,
+) -> Generator[pd.Series, None, None]:
+    """Read YouTube Video external traffic data from BigQuery.
+
+    Args:
+        bigquery_suffix (str): BigQuery dataset/table suffix.
+        start_date (dt.date): Date of earliest data to
+          request. This date refers to the partition field value, not the date
+          of the data itself.
+        end_date (Optional[dt.date], optional): Date of latest data to
+          request. This date refers to the partition field value, not the date
+          of the data itself. Defaults to None. Will be set to today if None.
+
+    Yields:
+        pd.Series: BigQuery response data.
+    """
+
+    if end_date is None:
+        end_date = utils.local_today()
+
+    query = """
+SELECT
+  `video_id`,
+  `traffic_source_detail`,
+  SUM(`views`) AS `views`,
+  SUM(`watch_time_minutes`) AS `watch_time_minutes`
+FROM
+  `@table_name`
+WHERE
+  DATE(_PARTITIONTIME) >= @start_date
+  AND DATE(_PARTITIONTIME) <= @end_date
+  AND `traffic_source_type` = 9
+  AND `traffic_source_detail` IS NOT NULL
+  AND `video_id` IS NOT NULL
+GROUP BY
+  `video_id`,
+  `traffic_source_detail`
+    """.strip()
+
+    query = insert_table_name(query, "p_channel_traffic_source_a2_", bigquery_suffix)
+
+    job_config = bigquery.QueryJobConfig(
+        default_dataset=f"wdr-okr.youtube_channel_{bigquery_suffix}",
+        query_parameters=[
+            bigquery.ScalarQueryParameter("start_date", "DATE", start_date.isoformat()),
+            bigquery.ScalarQueryParameter("end_date", "DATE", end_date.isoformat()),
+        ],
+    )
+
+    def df_cleaner(df: pd.DataFrame) -> pd.DataFrame:
+        return df.replace({np.nan: None})
 
     yield from iter_results(
         bigquery_client,
