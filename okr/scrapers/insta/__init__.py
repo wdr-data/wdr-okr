wdr-data/wdr-okr

commit 73817fbd8a21becc86e57fd60c9815e93786451c

🐛 Significantly increase the period of posts to scrape affects facebook, instagram, snapchat, tiktok and twitter

okr/scrapers/insta/quintly.py
@@ -207,7 +207,7 @@ def get_insta_igtv(
         "videoViews",
         "link",
     ]
-    start_date = start_date or datetime.date.today() - datetime.timedelta(days=7)
+    start_date = start_date or datetime.date.today() - datetime.timedelta(days=120)
     end_date = datetime.date.today()
     df = common_quintly.quintly.run_query(
         profile_ids, table, fields, start_date, end_date
@@ -251,7 +251,7 @@ def get_insta_comments(
         "isHidden",
         "importTime",
     ]
-    start_date = start_date or datetime.date.today() - datetime.timedelta(days=7)
+    start_date = start_date or datetime.date.today() - datetime.timedelta(days=120)
     end_date = datetime.date.today()
 
     df = common_quintly.quintly.run_query(

okr/scrapers/facebook/quintly.py
@@ -114,7 +114,7 @@ def get_facebook_posts(
         "is_published",
         "is_hidden",
     ]
-    start_date = start_date or datetime.date.today() - datetime.timedelta(days=7)
+    start_date = start_date or datetime.date.today() - datetime.timedelta(days=120)
     end_date = datetime.date.today()
 
     df_posts_insights = common_quintly.quintly.run_query(

okr/scrapers/snapchat_shows/quintly.py
@@ -140,7 +140,7 @@ def get_snapchat_show_stories(
         "uniqueTopsnapsPerUser",
         "importTime",
     ]
-    start_date = start_date or datetime.date.today() - datetime.timedelta(days=7)
+    start_date = start_date or datetime.date.today() - datetime.timedelta(days=120)
     end_date = datetime.date.today()
 
     df = common_quintly.quintly.run_query(
@@ -208,7 +208,7 @@ def get_snapchat_show_story_snaps(
         "topsnapTotalViews",
         "topsnapUniqueViews",
     ]
-    start_date = start_date or datetime.date.today() - datetime.timedelta(days=7)
+    start_date = start_date or datetime.date.today() - datetime.timedelta(days=120)
     end_date = datetime.date.today()
 
     df = common_quintly.quintly.run_query(

okr/scrapers/tiktok/quintly.py
@@ -115,7 +115,7 @@ def get_tiktok_posts(
 
     today = utils.local_today()
 
-    start_date = start_date or today - datetime.timedelta(days=365)
+    start_date = start_date or today - datetime.timedelta(days=120)
     end_date = today
 
     df = common_quintly.quintly.run_query(

okr/scrapers/twitter/quintly.py
@@ -99,7 +99,7 @@ def get_tweets(
         "retweets",
         "replies",
     ]
-    start_date = start_date or datetime.date.today() - datetime.timedelta(days=7)
+    start_date = start_date or datetime.date.today() - datetime.timedelta(days=120)
     end_date = datetime.date.today()
 
     df_posts_insights = common_quintly.quintly.run_query(

commit a3fa4e6200d165949437a8775296e78ea5931061

⚡️ Reduce RAM usage and time for insta comments Regression introduced with 73817fbd8a21becc86e57fd60c9815e93786451c

okr/scrapers/insta/__init__.py
@@ -3,12 +3,13 @@
 import datetime as dt
 import json
 from time import sleep
-from typing import Dict, Optional
+from typing import Dict, Generator, Optional
 
 from django.db.utils import IntegrityError
 from django.db.models import Q, Sum
 from loguru import logger
 from sentry_sdk import capture_exception
+from bulk_sync import bulk_sync
 
 from ...models.insta import (
     Insta,
@@ -398,15 +399,38 @@ def scrape_comments(
         try:
             _scrape_comments_insta(start_date, insta)
         except Exception as e:
+            logger.exception(f"Failed to scrape comments for {insta.name}")
             capture_exception(e)
 
 
 def _scrape_comments_insta(start_date, insta):
     logger.info(f"Scraping Instagram comments for {insta.name}")
-    df = quintly.get_insta_comments(insta.quintly_profile_id, start_date=start_date)
+    dfs = quintly.get_insta_comments(insta.quintly_profile_id, start_date=start_date)
 
     post_cache: Dict[str, InstaPost] = {}
 
+    for df in dfs:
+        comments = list(_scrape_comments_insta_day(insta, post_cache, df))
+
+        # bulk_sync breaks if there are no comments
+        if not comments:
+            continue
+
+        posts = set(comment.post for comment in comments)
+
+        sync_results = bulk_sync(
+            comments,
+            ["external_id"],
+            Q(post__in=posts),
+            batch_size=100,
+            skip_deletes=True,
+        )
+        logger.debug(sync_results)
+
+
+def _scrape_comments_insta_day(
+    insta: Insta, post_cache: Dict[str, InstaPost], df
+) -> Generator[InstaComment, None, None]:
     for index, row in df.iterrows():
         defaults = {
             "created_at": BERLIN.localize(dt.datetime.fromisoformat(row.time)),
@@ -441,18 +465,8 @@ def _scrape_comments_insta(start_date, insta):
 
         defaults["post"] = post
 
-        try:
-            obj, created = InstaComment.objects.update_or_create(
-                external_id=row.externalId,
-                defaults=defaults,
-            )
-        except IntegrityError as e:
-            capture_exception(e)
-            logger.exception(
-                "Data for comment with ID {} failed integrity check:\n{}",
-                row.externalId,
-                defaults,
-            )
+        # Build comment object for bulk_sync
+        yield InstaComment(external_id=row.externalId, **defaults)
 
 
 def scrape_demographics(

okr/scrapers/insta/quintly.py
@@ -1,10 +1,11 @@
 """ Methods for scraping insta data with Quintly """
 
 import datetime
-from typing import Optional
+from typing import Generator, Optional
 
 import numpy as np
 import pandas as pd
+from loguru import logger
 
 from ..common import quintly as common_quintly
 from ..common import utils
@@ -223,7 +224,7 @@ def get_insta_comments(
     profile_id: int,
     *,
     start_date: Optional[datetime.date] = None,
-) -> pd.DataFrame:
+) -> Generator[pd.DataFrame, None, None]:
     """Read data for comments on Instagram profile via Quintly API.
 
     Args:
@@ -251,16 +252,16 @@ def get_insta_comments(
         "isHidden",
         "importTime",
     ]
-    start_date = start_date or datetime.date.today() - datetime.timedelta(days=120)
-    end_date = datetime.date.today()
+    today = utils.local_today()
+    start_date = start_date or today - datetime.timedelta(days=120)
+    end_date = today
 
-    df = common_quintly.quintly.run_query(
-        profile_ids, table, fields, start_date, end_date
-    )
+    for date in reversed(utils.date_range(start_date, end_date)):
+        logger.trace("Getting next insta comment dataframe")
+        df = common_quintly.quintly.run_query(profile_ids, table, fields, date, date)
 
-    df = df.replace({np.nan: None})
-
-    return df
+        df.replace({np.nan: None}, inplace=True)
+        yield df
 
 
 @common_quintly.requires_quintly